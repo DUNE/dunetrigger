96ede04b8f5777701646ff27fbd56b21674d26db
Klaudia Wawrowska <klaudia.wawrowska@gmail.com>
2025-10-28 11:41:05 +0100

    updating summary tree to use ROP ID instead of view

--- dunetrigger/TriggerAna/TriggerAnaTree_module.cc
+++ dunetrigger/TriggerAna/TriggerAnaTree_module.cc
@@ -222,8 +222,8 @@ private:
   bool dump_summary_info;
   // visible energy for the event
   TTree *summary_tree;
-  double tot_visible_energy_U, tot_visible_energy_V, tot_visible_energy_X;
-  double tot_numelectrons_U, tot_numelectrons_V, tot_numelectrons_X;
+  double tot_visible_energy_0, tot_visible_energy_1, tot_visible_energy_2, tot_visible_energy_3; // total visible energy per readout plane ID
+  double tot_numelectrons_0, tot_numelectrons_1, tot_numelectrons_2, tot_numelectrons_3;
 
   std::map<std::string, std::array<int, 3>> bt_view_offsets;
 
@@ -347,12 +347,14 @@ void dunetrigger::TriggerAnaTree::beginJob() {
   if (dump_summary_info) {
     summary_tree = tfs->make<TTree>("event_summary", "event_summary");
     ev_buf.branch_on(summary_tree);
-    summary_tree->Branch("tot_visible_energy_U", &tot_visible_energy_U);
-    summary_tree->Branch("tot_visible_energy_V", &tot_visible_energy_V);
-    summary_tree->Branch("tot_visible_energy_X", &tot_visible_energy_X);
-    summary_tree->Branch("tot_numelectrons_U", &tot_numelectrons_U);
-    summary_tree->Branch("tot_numelectrons_V", &tot_numelectrons_V);
-    summary_tree->Branch("tot_numelectrons_X", &tot_numelectrons_X);
+    summary_tree->Branch("tot_visible_energy_0", &tot_visible_energy_0);
+    summary_tree->Branch("tot_visible_energy_1", &tot_visible_energy_1);
+    summary_tree->Branch("tot_visible_energy_2", &tot_visible_energy_2);
+    summary_tree->Branch("tot_visible_energy_3", &tot_visible_energy_3);
+    summary_tree->Branch("tot_numelectrons_0", &tot_numelectrons_0);
+    summary_tree->Branch("tot_numelectrons_1", &tot_numelectrons_1);
+    summary_tree->Branch("tot_numelectrons_2", &tot_numelectrons_2);
+    summary_tree->Branch("tot_numelectrons_3", &tot_numelectrons_3);
   }
 
   // Save detector settings
@@ -369,8 +371,8 @@ void dunetrigger::TriggerAnaTree::analyze(art::Event const &e) {
   ev_buf.event = e.event();
 
   // reset visible energy counters
-  tot_visible_energy_U = tot_visible_energy_V = tot_visible_energy_X = 0;
-  tot_numelectrons_U = tot_numelectrons_V = tot_numelectrons_X = 0;
+  tot_visible_energy_0 = tot_visible_energy_1 = tot_visible_energy_2 = tot_visible_energy_3 = 0;
+  tot_numelectrons_0 = tot_numelectrons_1 = tot_numelectrons_2 = tot_numelectrons_3 = 0;
 
   // get a service handle for geometry
   geo::WireReadoutGeom const *geom = &art::ServiceHandle<geo::WireReadout>()->Get();
@@ -472,17 +474,21 @@ void dunetrigger::TriggerAnaTree::analyze(art::Event const &e) {
           ide_readout_view = chinfo.view;
           ide_detector_element = chinfo.tpcset_id; // APA/CRP ID
           // populate the total visible energy counters by plane
-          if (ide_readout_view == geo::kU) {
-            tot_visible_energy_U += ide_energy;
-            tot_numelectrons_U += ide_numElectrons;
+          if (ide_readout_plane_id == 0) {
+            tot_visible_energy_0 += ide_energy;
+            tot_numelectrons_0 += ide_numElectrons;
           }
-          if (ide_readout_view == geo::kV) {
-            tot_visible_energy_V += ide_energy;
-            tot_numelectrons_V += ide_numElectrons;
+          else if (ide_readout_plane_id == 1) {
+            tot_visible_energy_1 += ide_energy;
+            tot_numelectrons_1 += ide_numElectrons;
           }
-          if (ide_readout_view == geo::kW) {
-            tot_visible_energy_X += ide_energy;
-            tot_numelectrons_X += ide_numElectrons;
+          else if (ide_readout_plane_id == 2) {
+            tot_visible_energy_2 += ide_energy;
+            tot_numelectrons_2 += ide_numElectrons;
+          }
+          else if (ide_readout_plane_id == 3) {
+            tot_visible_energy_3 += ide_energy;
+            tot_numelectrons_3 += ide_numElectrons;
           }
           simide_tree->Fill();
         }
