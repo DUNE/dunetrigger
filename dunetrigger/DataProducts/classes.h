a9cff28eb5428c437b55fd211e8c34c702f2abf4
Alessandro Thea <alessandro.thea@cern.ch>
2026-03-18 21:36:37 +0100

    Added a dummy data product to test compilation

---
+++ dunetrigger/DataProducts/classes.h
@@ -0,0 +1,4 @@
+// classes.h — includes every header whose classes appear in classes_def.xml
+#include "dunetrigger/DataProducts/MyProduct.h"
+#include "canvas/Persistency/Common/Wrapper.h"
+#include "canvas/Persistency/Common/Assns.h"
