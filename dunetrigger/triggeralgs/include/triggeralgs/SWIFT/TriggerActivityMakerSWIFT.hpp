04dec5fb39acb96773a3927482303dded30c909b
Klaudia Wawrowska <79635813+kwawrows@users.noreply.github.com>
2026-04-09 17:14:35 +0200

    Adding SWIFT TAMaker  (#43)
    * Porting SWIFT TAMaker from kwawrows/swift
    
    * addressing review comments
    
    * adding default alg. config to triggersim_makers.fcl
    
    * Adding comments to clarify config. variables
    
    * Adding sadc suffix to energy variables

---
+++ dunetrigger/triggeralgs/include/triggeralgs/SWIFT/TriggerActivityMakerSWIFT.hpp
@@ -0,0 +1,64 @@
+/*
+ * @file TriggerActivityMakerSWIFT.hpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2021.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#ifndef TRIGGERALGS_SWIFT_TRIGGERACTIVITYMAKERSWIFT_HPP_
+#define TRIGGERALGS_SWIFT_TRIGGERACTIVITYMAKERSWIFT_HPP_
+
+#include "dunetrigger/triggeralgs/include/triggeralgs/TriggerActivityFactory.hpp"
+#include <algorithm>
+
+namespace triggeralgs {
+
+  class TriggerActivityMakerSWIFT : public TriggerActivityMaker {
+  public:
+    void operator()(const TriggerPrimitive& input_tp, std::vector<TriggerActivity>& output_tas);
+    void configure(const nlohmann::json& config);
+    void set_ta_attributes();
+    void flush(timestamp_t until, std::vector<TriggerActivity>& output_tas) override;
+    bool preprocess( const TriggerPrimitive& input_tp) const;
+
+  private:
+    TriggerActivity m_current_ta;
+    
+    // -- Algortihm configuration --
+    // static variables which get set once at run start & kept fixed afterwards
+
+    //Window settings
+    uint64_t m_window_length;
+    uint64_t m_inspect_energy_threshold_sadc; 
+    uint64_t m_accept_energy_threshold_sadc;
+
+    //TP pre-processing
+    uint16_t m_min_adc_peak;
+    uint16_t m_min_samples_over_threshold;
+
+    //Clustering settings
+    float m_cm_per_tick;
+    float m_wire_pitch;
+    int   m_db_min_samples;
+    float m_db_eps;
+    uint64_t m_cluster_energy_cut_sadc;
+
+
+    // Dynamic vaiables which get updated during running to keep track of alg state. These are non-configurable
+
+    //window state
+    bool     m_initialised = false;
+    uint64_t m_window_start = 0;
+    uint64_t m_window_energy_sadc = 0;
+    uint16_t m_tp_count = 0;
+
+    //Additional SWIFT-specific functions and classes
+    enum class WindowDecision { kReject, kInspect, kAccept };
+    void close_window(std::vector<TriggerActivity>& output_tas);
+    void reset_window_state(uint64_t new_window_start);
+    uint64_t extract_dominant_cluster_energy(const std::vector<TriggerPrimitive>& tps, float eps, int min_samples);
+  };
+} // namespace triggeralgs
+
+#endif // TRIGGERALGS_SWIFT_TRIGGERACTIVITYMAKERSWIFT_HPP_
